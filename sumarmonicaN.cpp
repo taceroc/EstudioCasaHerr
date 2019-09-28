dab3912d43c43965c89214bc75cae93c6262f2e6
taceroc <taceroc@unal.edu.co>
2019-09-27 20:57:42 -0500

    Suma de 1/N al desde N = 1 a N = y otra de N = a N = 1

---
+++ sumarmonicaN.cpp
@@ -0,0 +1,41 @@
+#include<cstdio>
+#include<math.h>
+
+float S (int Nmax);
+float s (int Nmax);
+
+int main(void)
+
+{
+ 
+  int Nmax = 100;
+    for(int i = 1; i<=Nmax; i++)
+      {
+        float a = fabs((s(i)-S(i))/(S(i)+s(i)));
+	std::printf("%10d %20.16f %20.16f %20.16e\n", i, S(i), s(i),a);
+      }
+
+  return 0;
+}
+
+
+float S (int Nmax)
+{
+  float S = 0.0;
+  for(int N = 1; N<=Nmax; N++)
+    {
+      S = S + 1.0/N;
+    }
+  return S;
+}
+
+float s (int Nmax)
+{
+  float s = 0.0;
+    for(int N1 = Nmax; N1>=1 ; N1--)
+    {
+      s = s + 1.0/N1;
+    
+    }
+    return s; 
+}
