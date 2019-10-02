0a876d54cab626ee1b2090fe666ab95cfbea1c98
taceroc <taceroc@unal.edu.co>
2019-10-01 21:59:47 -0500

    no es necesario el while

--- overflowdouble1.cpp
+++ overflowdouble1.cpp
@@ -8,10 +8,10 @@
 int main(void)
 {
        
-       int N = 1090;
-       float under = 1.0, over = 1.0;
+       int N = 20000;
+       double under = 1.0, over = 1.0;
        
-       while(under > 0){
+      
        for (int ii = 0; ii < N; ++ii)
         {
              under /= 2.0;
@@ -21,9 +21,8 @@ int main(void)
                break;
 	      }
 	    }
-       }
-       while(isinf(over) != 1){
-        for (int ii = 0; ii < N; ++ii)
+
+       for (int ii = 0; ii < N; ++ii)
 	  {
                over *= 2.0;
                std::printf("%d \t %.16e \n", ii, over);
@@ -32,7 +31,7 @@ int main(void)
 	       break;
 	       }
 	  }
-       }
+       
        
     
 }
