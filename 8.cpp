90a56aaf66d2f9ca6776372256429eee00f0059c
taceroc <taceroc@unal.edu.co>
2019-10-01 21:34:47 -0500

    anadir comparacion

--- 8.cpp
+++ 8.cpp
@@ -2,28 +2,37 @@
 #include <stdio.h>
 #include<math.h>
 
+typedef double REAL;
 
-double f(double x);
+REAL f(REAL x);
+REAL g(REAL x);
 
 int main(void)
 
 {
-  double x = 1;
-  std::printf("f(%.16f) = %.16f\n", x, f(x));
+  REAL x ;
+  //std::printf("f(%.16f) = %.16f\n", x, f(x));
   int a = 0;
   
-  for(x = 1; x < 10; x = x-(x/10))
+  for(x = 1; x < 10.0; x = x/10.0 )
        {
-         std::printf("%d) f(%.16f) = %.16f\n",a, x, f(x));
+	 std::printf("%5d f(%.16f) = %.16f & g(%.16f) = %.16f\n",a, x, f(x), x ,g(x));
 	  a++;
 	  if(a==16)
 	    break;
        }      
 }
 
-double f(double x)
+REAL f(REAL x)
 {
-  double f;
+  REAL f;
   f = (x - sin(x))/ (pow(x,3));
   return f;
+}
+
+REAL g(REAL x)
+{
+  REAL g;
+  g = (pow(x,2) - pow(sin(x),2)) / (pow(x,3)*(x+sin(x)));
+  return g;
 }
