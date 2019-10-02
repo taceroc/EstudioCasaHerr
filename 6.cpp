43d3f3615ab406ca1bd9aa731ab22a95d3078b99
taceroc <taceroc@unal.edu.co>
2019-10-01 21:37:20 -0500

    tiene un cambio de variable que no siempre sirve

--- 6.cpp
+++ 6.cpp
@@ -1,39 +1,41 @@
 #include<cstdio>
 #include<math.h>
 
+typedef float REAL;
 
-double f(double x);
-double g (double x);
+REAL f(REAL x);
+REAL g(REAL x);
 
 int main (void)
 
 {
-  const double pi = 3.141592654;
-  double teta ;
-  double x;
+  const REAL pi = 3.141592654;
+  REAL teta ;
+  REAL x;
 
-  for(teta = 0; teta <= 1; teta=teta+0.0001)
+  for(teta = 0.0; teta <= 1.0; teta=teta+0.01)
     {
      x = teta * pi / 180;
-     std::printf("f(%.16f)= %.16f\n", x, f(x));
-     std::printf("g(%.16f)= %.16f\n", x, g(x));
-     double a = f(x)-g(x);
-     std::printf("f(%.16f)-g(%.16f)\n = %.16f\n",f(x),g(x),a);
+     //std::printf("f(%.16f)= %.16f\n", x, f(x));
+     //std::printf("g(%.16f)= %.16f\n", x, g(x));
+     REAL a = fabs((g(x)-f(x))/ f(x));
+     //std::printf("f(%.16f)-g(%.16f)\n = %.16f\n",f(x),g(x),a);
+     std:: printf ("%24.16f %24.16e %24.16e %24.16e\n",teta, f(x), g(x), a);
     }
 
   return 0;
 }
 
-double f(double x)
+REAL f(REAL x)
 {
-  double f;
-  f = sin (x) - 2 * pow(sin(x/2),2);
+  REAL f;
+  f = sin (x) - 2.0 * pow(sin(x/2),2);
   return f;
 }
 
-double g(double x)
+REAL g(REAL x)
 {
-  double g;
-  g = sin(x) - 1  + cos(x);
+  REAL g;
+  g = sin(x) - 1.0  + cos(x);
   return g;
 }
