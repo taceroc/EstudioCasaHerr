944f9d1be1db523ef15e19afada5939955f518a9
taceroc <taceroc@unal.edu.co>
2019-10-04 09:28:28 -0500

    6

--- 6.cpp
+++ 6.cpp
@@ -1,24 +1,24 @@
 #include<cstdio>
 #include<math.h>
 
-typedef float REAL;
+//typedef float REAL;
 
-REAL f(REAL x);
-REAL g(REAL x);
+float f(float x);
+float g(float x);
 
 int main (void)
 
 {
-  const REAL pi = 3.141592654;
-  REAL teta ;
-  REAL x;
+  const float pi = 3.141592654;
+  float teta ;
+  float x;
 
-  for(teta = 0.0; teta <= 1.0; teta=teta+0.01)
+  for(teta = 0.0; teta <= 45; teta=teta+1)
     {
      x = teta * pi / 180;
      //std::printf("f(%.16f)= %.16f\n", x, f(x));
      //std::printf("g(%.16f)= %.16f\n", x, g(x));
-     REAL a = fabs((g(x)-f(x))/ f(x));
+     float a = fabs((g(x)-f(x))/ f(x));
      //std::printf("f(%.16f)-g(%.16f)\n = %.16f\n",f(x),g(x),a);
      std:: printf ("%24.16f %24.16e %24.16e %24.16e\n",teta, f(x), g(x), a);
     }
@@ -26,16 +26,16 @@ int main (void)
   return 0;
 }
 
-REAL f(REAL x)
+float f(float x)
 {
-  REAL f;
+  float f;
   f = sin (x) - 2.0 * pow(sin(x/2),2);
   return f;
 }
 
-REAL g(REAL x)
+float g(float x)
 {
-  REAL g;
+  float g;
   g = sin(x) - 1.0  + cos(x);
   return g;
 }
