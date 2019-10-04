b0b0d6ca5738099dfdb2647fb313828e0f3c6fee
taceroc <taceroc@unal.edu.co>
2019-10-04 09:29:16 -0500

    8

--- 8.cpp
+++ 8.cpp
@@ -6,7 +6,7 @@ typedef double REAL;
 
 REAL f(REAL x);
 REAL g(REAL x);
-
+REAL h(REAL x);
 int main(void)
 
 {
@@ -14,9 +14,9 @@ int main(void)
   //std::printf("f(%.16f) = %.16f\n", x, f(x));
   int a = 0;
   
-  for(x = 1; x < 10.0; x = x/10.0 )
+  for(x = 1; x < 100.0; x = x/10.0 )
        {
-	 std::printf("%5d f(%.16f) = %.16f & g(%.16f) = %.16f\n",a, x, f(x), x ,g(x));
+	 std::printf("%5d f(%.16f) = %.16f & g(%.16f) = %.16f & h(%.16f) = %.16f\n",a, x, f(x), x ,g(x),x,h(x));
 	  a++;
 	  if(a==16)
 	    break;
@@ -36,3 +36,11 @@ REAL g(REAL x)
   g = (pow(x,2) - pow(sin(x),2)) / (pow(x,3)*(x+sin(x)));
   return g;
 }
+
+REAL h(REAL x)
+{
+  REAL h;
+  h = (1/(x*x)) * (1 - (sin(x)*sin(x)/(x*x)))/(1+(sin(x)/x));
+  return h;
+}
+
